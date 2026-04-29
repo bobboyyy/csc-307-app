d64221716c32ee7727e7d48f271c6c970c09f88d
bobboyyy <147883245+bobboyyy@users.noreply.github.com>
2026-04-29 10:38:35 -0700

    finsihed up to task 3 - IA 4

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,6 +1,8 @@
 // backend.js
 import express from "express";
 import cors from "cors";
+import userService from "./user-services.js";
+
 
 const app = express();
 const port = 8000;
@@ -10,123 +12,65 @@ app.use(cors());
 
 app.use(express.json());
 
-const findUserByName = (name) => {
-    return users["users_list"].filter(
-        (user) => user["name"] === name
-    );
-};
-
 app.get("/users", (req, res) => {
     const name = req.query.name;
     const job = req.query.job;
-    if (name != undefined) {
-        let result = findUserByName(name);
-        if (job != undefined) {
-            result = result.filter(
-                (user) => user["job"] === job);
-        }
-        result = { users_list: result };
-        res.send(result);
-    } else {
-        res.send(users);
-    }
+    userService.getUsers(name, job)
+        .then((result) => {
+            res.send({ users_list: result });
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).send("Internal Server Error");
+        });
 });
-
-
-const findUserById = (id) =>
-    users["users_list"].find((user) => user["id"] === id);
-
-
 
 app.get("/users/:id", (req, res) => {
-    const id = req.params["id"]; //or req.params.id
-    let result = findUserById(id);
-    if (result === undefined) {
-        res.status(404).send("Resource not found.");
-    } else {
-        res.send(result);
-    }
+    const id = req.params.id;
+    userService.findUserById(id)
+        .then((result) => {
+            if (!result) {
+                res.status(404).send("Resource not found.");
+            } else {
+                res.send(result);
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).send("Internal Server Error");
+        });
 });
-
-const generateId = () => Math.floor(Math.random() * 1000000).toString();
-
-const addUser = (user) => {
-    user.id = generateId();
-    users["users_list"].push(user);
-    return user;
-};
 
 app.post("/users", (req, res) => {
     const userToAdd = req.body;
-    const newUser = addUser(userToAdd);
-    res.status(201).send(newUser); // 201 + created object ID
+    userService.addUser(userToAdd)
+        .then((savedUser) => {
+            res.status(201).send(savedUser);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).send("Internal Server Error");
+        });
 });
-
-const removeUserById = (id) => {
-    const index = users["users_list"].findIndex(
-        (user) => user["id"] === id
-    );
-    if (index !== -1) {
-        users["users_list"].splice(index, 1);
-        return true;
-    }
-    return false;
-};
-
 
 app.delete("/users/:id", (req, res) => {
-    const id = req.params["id"];
-    if (removeUserById(id)) {
-        res.status(204).send();
-    } else {
-        res.status(404).send("Resource not found.");
-    }
+    const id = req.params.id;
+    userService.removeUserById(id)
+        .then((result) => {
+            if (!result) {
+                res.status(404).send("Resource not found.");
+            } else {
+                res.status(204).send();
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).send("Internal Server Error");
+        });
 });
-
-
 
 app.listen(port, () => {
     console.log(
         `Example app listening at http://localhost:${port}`
     );
-});
-
-const users = {
-    users_list: [
-        {
-            id: "xyz789",
-            name: "Charlie",
-            job: "Janitor"
-        },
-        {
-            id: "abc123",
-            name: "Mac",
-            job: "Bouncer"
-        },
-        {
-            id: "ppp222",
-            name: "Mac",
-            job: "Professor"
-        },
-        {
-            id: "yat999",
-            name: "Dee",
-            job: "Aspring actress"
-        },
-        {
-            id: "zap555",
-            name: "Dennis",
-            job: "Bartender"
-        },
-        {
-            id: "b30101",
-            name: "bob",
-            job: "goated"
-        },
-        {
-            id: "062729",
-            name: "hay",
-            job: "yuhh"
-        }
-    ]
-};
+});
